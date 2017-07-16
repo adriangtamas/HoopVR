3a442426425af823cb3c18eda746a3d20f20c5bb
AT <adriangtamas@gmail.com>
2017-07-16 20:04:33 +0300

    SH04 Persisting Data Between Scenes

---
+++ Assets/Scripts/GameOverScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverScoreDisplay : MonoBehaviour
+{
+
+	Text text;
+	ScoreAbsorber scoreAbsorber;
+
+	// Use this for initialization
+	void Start()
+	{
+		text = GetComponent<Text>();
+		scoreAbsorber = FindObjectOfType<ScoreAbsorber>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		int currentScore = scoreAbsorber.score;
+		text.text = "Score: " + currentScore;
+	}
+}
