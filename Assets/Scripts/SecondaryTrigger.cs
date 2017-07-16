500bd3d1f4761b551bc18362571e863091ad046d
AT <adriangtamas@gmail.com>
2017-07-16 22:48:51 +0300

    SH07 Triggering Sounds - Version Alpha

---
+++ Assets/Scripts/SecondaryTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondaryTrigger : MonoBehaviour {
+
+	Collider expectedCollider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ExpectCollider(Collider collider)
+	{
+		expectedCollider = collider;
+	
+	}
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if (otherCollider == expectedCollider)
+		{
+			ScoreKeeper scoreKeep = FindObjectOfType<ScoreKeeper>();
+			scoreKeep.IncrementScore(1);
+		}
+	}
+}
